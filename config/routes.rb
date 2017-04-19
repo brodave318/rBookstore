d56202040058c8f684de1b2312d1a24c5ad50567
David <brodave318@gmail.com>
2017-04-18 17:26:38 -0700

    Configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -57,5 +57,12 @@ Rails.application.routes.draw do
 
   get 'about/index'
 
+  root 'books#index'
+
+  resources :books
+  resources :authors
+  resources :categories
+  resources :publishers
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
