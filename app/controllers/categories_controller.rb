f0a9cdbc5803719dd9e1e2e3ebce635736816bd5
David <brodave318@gmail.com>
2017-04-29 12:00:22 -0700

    Created category show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -30,6 +30,8 @@ class CategoriesController < ApplicationController
 
   def show
     @categories = Category.all
+    @category = Category.find(params[:id])
+    @books = @category.books
   end
 
   private
