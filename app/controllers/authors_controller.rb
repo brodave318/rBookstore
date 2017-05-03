9bbf5a1bfe4f263da7874f838ddccc8c0543fe8c
David <brodave318@gmail.com>
2017-05-02 17:51:39 -0700

    Added 'Manage' drop-down menu | Edit, delete & update functions for categories, authors & publishers possible

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,7 +8,7 @@ class AuthorsController < ApplicationController
     @author = Author.new(author_params)
     if @author.save
       flash[:notice] = "Author Created"
-      
+
       redirect_to authors_path
     else
       render 'new'
@@ -16,18 +16,31 @@ class AuthorsController < ApplicationController
   end
 
   def update
+    @author = Author.find(params[:id])
+
+    @author.update(author_params)
+
+    flash[:notice] = 'Author Updated'
+
+    redirect_to authors_path
   end
 
   def edit
+    @author = Author.find(params[:id])
   end
 
   def destroy
+    @author = Author.find(params[:id])
+
+    @author.destroy
+
+    flash[:notice] = 'Author Removed'
+
+    redirect_to authors_path
   end
 
   def index
-  end
-
-  def show
+    @authors = Author.all
   end
 
   private
